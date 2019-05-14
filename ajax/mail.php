04009e4438781ee425517f53e46bfb992709ae8b
roman <19roman23@gmail.com>
2019-05-14 19:31:57 +0300

    create mail.php

--- ajax/mail.php
+++ ajax/mail.php
@@ -2,9 +2,10 @@
     $name = $_POST['name'];
     $email = $_POST['mail'];
     $phone = $_POST['phone'];
-    $mesag = $_POST['mesag'];
+    $message = $_POST['mesag'];
 
-
+    $subject = "=?utf-8?B?".base64_encode("Повідомлення із сайту декору.")."?=";
+    $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
 
 
     $success = mail("loren.decor@ukr.net", $subject, $message, $headers);
