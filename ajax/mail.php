2b68f87ccdf3a5c9d1218ab8fabad495ad9d9a68
roman <19roman23@gmail.com>
2019-05-29 20:16:16 +0300

    replace foto in page galery

--- ajax/mail.php
+++ ajax/mail.php
@@ -9,13 +9,13 @@ $mail = trim(strip_tags($_POST['mail']));
 $phone = trim(strip_tags($_POST['phone']));
 $massage = trim(strip_tags($_POST['mesag']));
 
-$subject = "=?utf-8?B?" . base64_encode("Повідомлення із сайту декору.") . "?=";
+$subject = "=?utf-8?B?".base64_encode("Повідомлення із сайту декору.")."?=";
 $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
 
 $text = $name + '  ' + $phone + '  ' + $message;
 
 
-$success = mail("loren.decor@ukr.net", $subject, $text, $headers);
+$success = mail("loren.decor@ukr.net", $subject, $massage, $headers);
 echo $success;
 
 ?>
